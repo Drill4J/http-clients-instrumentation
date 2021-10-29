d7fbcdc33ad90dd555cda2a5dee0bd2b8b2f41e9
Ivanov Roman <40431224+Mazel-Tovr@users.noreply.github.com>
2021-10-29 14:35:02 +0300

    EPMDJ-8544 Create library with instrumentation of clients (#1)
    * Initial commit
    
    * EPMDJ-8544 Add github actions
    
    * EPMDJ-8544 Add poc
    
    * EPMDJ-8544 Add JavaHttpUrlConnection responce instrumentation
    
    * EPMDJ-8544 Add loging and check for callback set
    
    * EPMDJ-8544 Fix github actions
    
    * EPMDJ-8544 Add readme
    
    * EPMDJ-8544 Replace javassist dependency

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import com.hierynomus.gradle.license.tasks.*
+import java.net.*
+
+plugins {
+    kotlin("jvm")
+    id("com.github.hierynomus.license")
+    `maven-publish`
+}
+
+val ttlVersion: String by extra
+val drillLoggerVersion: String by extra
+val scriptUrl: String by extra
+val knasmVersion: String by extra
+val javassistVersion: String by extra
+
+apply(from = rootProject.uri("$scriptUrl/git-version.gradle.kts"))
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    apply(from = "$scriptUrl/maven-repo.gradle.kts")
+}
+
+
+
+dependencies {
+    implementation("org.javassist:javassist:$javassistVersion")
+    implementation("com.epam.drill.logger:logger:$drillLoggerVersion")
+    implementation("com.epam.drill.knasm:knasm:$knasmVersion")
+}
+
+val licenseFormatSettings by tasks.registering(LicenseFormat::class) {
+    source = fileTree(project.projectDir).also {
+        include("**/*.kt", "**/*.java", "**/*.groovy")
+        exclude("**/.idea")
+    }.asFileTree
+}
+
+license {
+    headerURI = URI("https://raw.githubusercontent.com/Drill4J/drill4j/develop/COPYRIGHT")
+}
+
+tasks["licenseFormat"].dependsOn(licenseFormatSettings)
+
+
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("publish-instrumentation") {
+            artifact(tasks.jar.get())
+            artifactId = rootProject.name
+            artifact(sourcesJar.get())
+        }
+    }
+}
